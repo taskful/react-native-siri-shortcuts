2654336b855c3a1d408fd1bfb8927268350f1a8a
Michael Hirsch <michael@hirsch.me>
2018-09-24 13:48:42 -0500

    Updated package.json

--- RNSiriShortcuts.h
+++ RNSiriShortcuts.h
@@ -1,5 +1,9 @@
-#import "RCTBridgeModule.h"
 #import <Foundation/Foundation.h>
+#if __has_include(<React/RCTBridgeModule.h>)
+#import <React/RCTBridgeModule.h>
+#else
+#import "RCTBridgeModule.h"
+#endif
 
 @interface RNSiriShortcuts : NSObject <RCTBridgeModule>
 
